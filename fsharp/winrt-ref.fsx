5293ebba73f007cccc134e8366ace3230594f9b7
7shi <7shi@live.jp>
2020-06-24 20:16:31 +0900

    modified:   fsharp/winrt-ref.fsx

--- fsharp/winrt-ref.fsx
+++ fsharp/winrt-ref.fsx
@@ -4,6 +4,6 @@
 //#r @"C:\Program Files (x86)\Windows Kits\10\UnionMetadata\Facade\Windows.WinMD"
 //#r @"C:\Program Files (x86)\Windows Kits\10\UnionMetadata\10.0.17763.0\Windows.winmd"
 #r @"C:\Program Files (x86)\Windows Kits\10\UnionMetadata\10.0.17763.0\Facade\windows.winmd"
-#r @"C:\Program Files (x86)\Windows Kits\10\References\10.0.17763.0\Windows.Foundation.FoundationContract/3.0.0.0/Windows.Foundation.FoundationContract.winmd"
+#r @"C:\Program Files (x86)\Windows Kits\10\References\10.0.17763.0\Windows.Foundation.FoundationContract\3.0.0.0\Windows.Foundation.FoundationContract.winmd"
 #r @"C:\Program Files (x86)\Windows Kits\10\References\10.0.17763.0\Windows.Foundation.UniversalApiContract\7.0.0.0\Windows.Foundation.UniversalApiContract.winmd"
 let voices = Windows.Media.SpeechSynthesis.SpeechSynthesizer.AllVoices
